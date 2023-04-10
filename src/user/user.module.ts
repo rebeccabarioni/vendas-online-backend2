4b52b41f718c30b8d41934773172f06dc24049ad
Rebecca Barioni Oliveira <130078970+rebeccabarioni@users.noreply.github.com>
2023-04-10 20:09:47 -0300

    feat: creat user (POST)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
+import { UserController } from './user.controller.js';
 
 @Module({
   controllers: [UserController]
