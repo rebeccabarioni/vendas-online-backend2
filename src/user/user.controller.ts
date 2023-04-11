5bf544299096660fc91a860371d6247cc7f9a107
Rebecca Barioni Oliveira <130078970+rebeccabarioni@users.noreply.github.com>
2023-04-10 21:03:04 -0300

    feat:service no nest.js

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,19 @@
-import {Body, Controller, Post } from '@nestjs/common';
+import {Body, Controller, Get, Post } from '@nestjs/common';
 import {createUserDto} from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
-    
+
+    constructor(private readonly userService: UserService) {}
     @Post()
-    async createUser(@Body() createUser: createUserDto) {
-      return{
-        ...createUser,
-        password: undefined,
-      };
+    async createUser(@Body() createUser: createUserDto): Promise<User> {
+      return this.userService.createUser(createUser);
+    }
+
+    @Get()
+    async getAllUser(): Promise<User[]> {
+        return this.userService.getAllUser();
     }
 }
